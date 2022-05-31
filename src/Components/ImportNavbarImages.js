ef877533f239394079c5484b2f197b7d815c93fb
limonatasuyu <emredilek6@gmail.com>
2022-05-31 20:11:37 +0300

    Finished Navbar Layout

---
+++ src/Components/ImportNavbarImages.js
@@ -0,0 +1,51 @@
+import {ReactComponent as Explore} from '../img/NavbarIcons/01/explore.svg'
+import {ReactComponent as ExploreActive} from '../img/NavbarIcons/01/explore_active.svg'
+import {ReactComponent as Home} from '../img/NavbarIcons/01/home.svg'
+import {ReactComponent as HomeActive} from '../img/NavbarIcons/01/home_active.svg'
+import {ReactComponent as Shorts} from '../img/NavbarIcons/01/shorts.svg'
+import {ReactComponent as ShortsActive} from '../img/NavbarIcons/01/shorts_active.svg'
+import {ReactComponent as Subs} from '../img/NavbarIcons/01/subscriptions.svg'
+import {ReactComponent as SubsActive} from '../img/NavbarIcons/01/subscriptions_active.svg'
+
+import {ReactComponent as History} from '../img/NavbarIcons/02/history.svg'
+import {ReactComponent as HistoryActive} from '../img/NavbarIcons/02/history_active.svg'
+import {ReactComponent as Library} from '../img/NavbarIcons/02/library.svg'
+import {ReactComponent as Likes} from '../img/NavbarIcons/02/likes.svg'
+import {ReactComponent as LikesActive} from '../img/NavbarIcons/02/likes_active.svg'
+import {ReactComponent as List} from '../img/NavbarIcons/02/list.svg'
+import {ReactComponent as ListActive} from '../img/NavbarIcons/02/list_active.svg'
+import {ReactComponent as WatchLater} from '../img/NavbarIcons/02/watchLater.svg'
+import {ReactComponent as WatchLaterActive} from '../img/NavbarIcons/02/watchLater_active.svg'
+import {ReactComponent as YourVideos} from '../img/NavbarIcons/02/yourVideos.svg'
+
+import {ReactComponent as Game} from '../img/NavbarIcons/04/game.svg'
+import {ReactComponent as GameActive} from '../img/NavbarIcons/04/game_active.svg'
+import {ReactComponent as Live} from '../img/NavbarIcons/04/live.svg'
+import {ReactComponent as LiveActive} from '../img/NavbarIcons/04/live_active.svg'
+import {ReactComponent as Premium} from '../img/NavbarIcons/04/premium.svg'
+import {ReactComponent as Sports} from '../img/NavbarIcons/04/sports.svg'
+import {ReactComponent as SportsActive} from '../img/NavbarIcons/04/sports_active.svg'
+
+import {ReactComponent as Feedback} from '../img/NavbarIcons/05/feedback.svg'
+import {ReactComponent as Help} from '../img/NavbarIcons/05/help.svg'
+import {ReactComponent as ReportHistory} from '../img/NavbarIcons/05/reportHistory.svg'
+import {ReactComponent as ReportHistoryActive} from '../img/NavbarIcons/05/reportHistory_active.svg'
+import {ReactComponent as Settings} from '../img/NavbarIcons/05/settings.svg'
+
+import {ReactComponent as Navigate} from '../img/NavbarIcons/navigate.svg'
+
+import Avatar from '../img/OtherIcons/avatar.jpeg'
+
+export {
+	Explore, ExploreActive, Home, HomeActive, Shorts, ShortsActive, Subs, SubsActive,
+	
+	History, HistoryActive, Library, Likes, LikesActive, List, ListActive, WatchLater, WatchLaterActive, YourVideos,
+	
+	Game, GameActive, Live, LiveActive, Premium, Sports, SportsActive,
+	
+	Feedback, Help, ReportHistory, ReportHistoryActive, Settings,
+	
+	Navigate,
+	
+	Avatar,
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Header from '../Components/Header'
+import Navbar from '../Components/Navbar'
 
 export default function HomePage() {
 	return(
 		<div className="homepage-container" >
 			<Header />
+			<Navbar />
 		</div>
 	)
 }
