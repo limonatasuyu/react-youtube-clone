dee8802c6711d487f4dae3346eb7fc93bf3e2214
limonatasuyu <emredilek6@gmail.com>
2022-06-06 19:39:13 +0300

    Finished videos's layout on homepage

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -2,19 +2,20 @@ import videoImg from '../img/example-thumbnail.webp'
 import Avatar from '../img/OtherIcons/avatar.jpeg'
 import {ReactComponent as Options} from '../img/DropDownIcons/VideoOptions/options.svg'
 
+
 function VideoThumbnail() {
 	return(
 		<div className="video-thumbnail">
 				<div className="video-img-wrapper">
-					<img src={videoImg}/>
+					<img src={videoImg} alt=""/>
 					<div className="video-duration">0:00</div>
 				</div>
 				<div className="video-details flex">
 					<div className="channel-img--thumbnail">
-						<img src={Avatar}/>
+						<img src={Avatar} alt=""/>
 					</div>
 					<div className="video-details-text">
-						<p className="video-name">“Its just a stoopid kids movie”</p>
+						<p className="video-name">Web Development Has Gone Crazy aa bb cc dd ee ff gg hh ii jj kk ll mm nn oo pp rr ss tt uu</p>
 						<p className="channel-name">Carsen Strange</p>
 						<p className="video-details-meta">281K views • 1 month ago </p>
 					</div>
